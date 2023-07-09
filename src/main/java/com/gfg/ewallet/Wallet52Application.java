81fcdf380131c527c6c7dcaba595604e5ca39e82
meet-gfg <Desire@4241>
2023-07-09 16:21:23 +0530

    initial

---
+++ src/main/java/com/gfg/ewallet/Wallet52Application.java
@@ -0,0 +1,13 @@
+package com.gfg.ewallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Wallet52Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Wallet52Application.class, args);
+	}
+
+}
